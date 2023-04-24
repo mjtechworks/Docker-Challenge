cdab2e7a1c2a5fe408560c5bfdf78a0459b216db
mjtechworks <michaeljohnson00518@gmail.com>
2023-04-23 21:08:29 -0700

    complete

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -4,13 +4,18 @@ from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 @app.get("/api/test_users/{user_id}")
